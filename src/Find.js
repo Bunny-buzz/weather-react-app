2e2b0325fa0346b56c03c13126cc1d3afff191ba
Bunny-buzz <Bunny-buzz@users.noreply.github.com>
2020-09-05 21:41:39 -0600

    Changed back to <a>

--- src/Find.js
+++ src/Find.js
@@ -6,39 +6,6 @@ export default function Search(props) {
   const [city, setCity] = useState(" ");
   const [message, setMessage] = useState("");
 
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[date.getDay()];
-
-  return `${day} ${hours}:${minutes}`;
-
-  let date = new Date(timestamp);
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  return `${hours}:${minutes}`;
-
   function displayTemperature(response) {
     console.log(response);
     setMessage(response.data);
--- src/Footer.js
+++ src/Footer.js
@@ -3,13 +3,13 @@ import React from "react";
 export default function Footer(props) {
   return (
     <small className="source">
-      <button
+      <a
         href="https://github.com/Bunny-buzz/weather-react-app"
         target="_blank"
         rel="noopener noreferrer"
       >
         Open-source code
-      </button>
+      </a>
       by Nimi Dada
     </small>
   );
